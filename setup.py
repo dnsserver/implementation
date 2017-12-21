9678802377fe36b267aa0856275e7c45611a8686
Denis Babani <denis@Deniss-MBP.fios-router.home>
2017-12-21 05:52:23 -0500

    adding first files

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opal3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'requests',
+        'flask',
+        'flask_sqlalchemy',
+        'flask_admin',
+        'flask_oidc',
+        'flask_bootstrap',
+        'flask_nav',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
+
